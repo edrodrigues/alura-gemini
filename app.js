d13cde06195cdd43b063b5eadad71e7053f231a0
Edmilson Rodrigues <ed.ufpe@gmail.com>
2024-09-05 10:39:36 -0300

    Adiciona conteúdo até a aula 4 da imersão.

---
+++ app.js
@@ -0,0 +1,23 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+let resultados = "";
+
+// para cada dado dentro da lista de dados 
+
+for (let dado of dados) { 
+    resultados += `
+        <div class="item-resultado"> 
+            <h2>
+            <a href="#" target = "_blank"> ${dado.titulo} </a>
+            </h2>
+        <p class="descrição-meta">${dado.descricao} </p>
+        <a href=${dado.link} target="_blank> Mais Informações </a>
+        </div>
+    `;
+}
+
+section.innerHTML = resultados;
+
+}
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: 'Rayssa Leal, conhecida como "Fadinha do Skate", é uma skatista brasileira que ganhou a medalha de prata nas Olimpíadas de Tóquio 2020 e a medalha de bronze nas Olimpíadas de Paris 2024.', 
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    }, 
+    {
+        titulo: "Beatriz Souza",
+        descricao: 'Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris 2024, tornando-se um dos maiores nomes do judô feminino brasileiro.',
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" // Substitua por um link válido para a página da Wikipedia de Beatriz Souza
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: 'Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições internacionais, incluindo a medalha de prata no individual geral nas Olimpíadas de Tóquio 2020.',
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" // Substitua por um link válido para a página da Wikipedia de Rebeca Andrade
+    }
+];
